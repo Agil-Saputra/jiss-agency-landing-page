aff4d27ba932b112e621d0703809112e6474cd88
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-03-23 19:02:41 +0700

    create contact and footer

--- components/contact.jsx
+++ components/contact.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Button2 from "./ui/button2";
 
 export default function contact() {
   return (
-    <div>contact</div>
-  )
+    <section className="bg-primary rounded-[2rem] py-10 text-center mt-32 px-4 placeholder flex-center flex-col">
+      <h2 className="h2 text-white max-w-[26ch]">
+        Time to change your lifestyle to find health services. join us for any
+        updates on medicare
+      </h2>
+      <p className="text-[22px] my-10 max-w-[45ch] text-white opacity-90 font-[300]">
+      Begin your wellness journey in a structured way by 
+consulting with the best doctors available on medicare.
+      </p>
+      <Button2 title="Join us now!" href="" style="bg-white border-transparent shadow-lg hover:border-white"/>
+    </section>
+  );
 }
--- components/ui/button2.jsx
+++ components/ui/button2.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function button2({title, href}) {
+export default function button2({title, href, style}) {
   return (
-    <a href={href} className="max-sm:w-full smooth text-[18px] border-[2px] text-primary font-bold border-primary px-6 py-2 rounded-3xl hover:bg-primary hover:text-white">{title}</a>
+    <a href={href} className={"max-sm:w-full smooth text-[18px] border-[2px] text-primary font-bold  px-6 py-2 rounded-3xl hover:bg-primary hover:text-white " + style}>{title}</a>
   )
 }
