91c229fa0da01985b2a84263ada61efa93037b0a
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-07-07 11:24:34 +0700

    update download link

--- pages/mitra/[slug].js
+++ pages/mitra/[slug].js
@@ -33,13 +33,21 @@ export async function getStaticProps({ params }) {
 }
 
 const Mitra = ({ mitra }) => {
-	const { judulProduk, gambarIlustrasi, deskripsi, brosur, infoDetail, welcomeText, infoPentingRefrensi } = mitra.fields;
+  const {
+    judulProduk,
+    gambarIlustrasi,
+    deskripsi,
+    brosur,
+    infoDetail,
+    welcomeText,
+    infoPentingRefrensi,
+    formulir,
+  } = mitra.fields;
 
+  console.log(formulir.fields.file.url);
   return (
-    <Navbar>  
-		<h2 className="h2 mb-6">
-		  {welcomeText}
-		</h2>
+    <Navbar>
+      <h2 className="h2 mb-6">{welcomeText}</h2>
       <p className="md:text-[32px] text-primary text-[15px]">{judulProduk}</p>
       <div>
         <div className="lg:float-right lg:ml-4 max-lg:my-4 max-md:w-full">
@@ -55,45 +63,70 @@ const Mitra = ({ mitra }) => {
       </div>
 
       <div>
-		{infoDetail.map((item, i) => {
-			const {judulInfo, infoDetail} = item.fields
-			return (
-				<div key={i} className="mb-2">
-					<h3 className="lg:text-3xl text-[1.2rem] font-bold">{judulInfo} : </h3>
-					<ul>
-						{infoDetail.map((item, i) => <li className="p" key={i}> - {item}</li>)}
-					</ul>
-				</div>
-			)
-		})}
+        {infoDetail.map((item, i) => {
+          const { judulInfo, infoDetail } = item.fields;
+          return (
+            <div key={i} className="mb-2">
+              <h3 className="lg:text-3xl text-[1.2rem] font-bold">
+                {judulInfo} :{" "}
+              </h3>
+              <ul>
+                {infoDetail.map((item, i) => (
+                  <li className="p" key={i}>
+                    {" "}
+                    - {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          );
+        })}
       </div>
 
       <div>
         <h2 className="h2 capitalize mb-6">Informasi Penting & Referensi</h2>
-        {
-			infoPentingRefrensi.map((item, i) => {
-				const {judulRefrensi, ilustrasiRefrensi, websiteReferensi, fileRefrensi} = item.fields
-				return (
-					<div key={i}>
-						<h3 className="lg:text-3xl text-[1.2rem] font-bold my-4">{judulRefrensi}</h3>
-						<Image 
-							src={`https:${ilustrasiRefrensi.fields.file.url}`}
-							width={400}
-							height={400}
-							alt="ilustrasi refrensi"
-							className="rounded-md mb-4"
-						/>
-						{websiteReferensi ? <a href={websiteReferensi} className="underline text-primary mt-3">Info Lebih lanjut Klik Disini</a>: null}
-						{fileRefrensi ? <iframe src={`https:${fileRefrensi.fields.file.url}`} className="h-screen w-full">Info Lebih lanjut Klik Disini</iframe>: null}
-
-					</div>
-				)
-			})
-		}
+        {infoPentingRefrensi.map((item, i) => {
+          const {
+            judulRefrensi,
+            ilustrasiRefrensi,
+            websiteReferensi,
+            fileRefrensi,
+          } = item.fields;
+          return (
+            <div key={i}>
+              <h3 className="lg:text-3xl text-[1.2rem] font-bold my-4">
+                {judulRefrensi}
+              </h3>
+              <Image
+                src={`https:${ilustrasiRefrensi.fields.file.url}`}
+                width={400}
+                height={400}
+                alt="ilustrasi refrensi"
+                className="rounded-md mb-4"
+              />
+              {websiteReferensi ? (
+                <a
+                  href={websiteReferensi}
+                  className="underline text-primary mt-3"
+                >
+                  Info Lebih lanjut Klik Disini
+                </a>
+              ) : null}
+              {fileRefrensi ? (
+                <iframe
+                  src={`https:${fileRefrensi.fields.file.url}`}
+                  className="h-screen w-full"
+                >
+                  Info Lebih lanjut Klik Disini
+                </iframe>
+              ) : null}
+            </div>
+          );
+        })}
       </div>
 
       <div>
-        <h2 className="h2 capitalize mb-6">Brosur</h2>
+        <h2 className="h2 capitalize mb-6 mt-4">Brosur</h2>
         <div className="flex flex-wrap gap-4">
           <Image
             src={`https:${brosur.fields.file.url}`}
@@ -104,10 +137,10 @@ const Mitra = ({ mitra }) => {
         </div>
       </div>
 
-      <div>
+      <div className="mb-10">
         <h2 className="h2 capitalize my-6">Download Formulir</h2>
-        <a href="" download>
-          Download
+        <a href={`https:${formulir.fields.file.url}`} className="underline text-primary" download>
+          Download Formulir Disini
         </a>
       </div>
     </Navbar>
