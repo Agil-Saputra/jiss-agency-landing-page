f4da5ad3cd016f3c43fceb71bc7477fe0bf85ea2
Agil <88566690+Agil-Saputra@users.noreply.github.com>
2023-07-18 14:45:14 +0700

    add bg loading

--- components/hero/videoBackground/video.jsx
+++ components/hero/videoBackground/video.jsx
@@ -1,11 +1,8 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function VideoBackground({videoSrc}) {
-
-	// const [load, setLoad] = useState(true)∏
-
   return (
-    <video className="absolute top-0 -z-10 w-full md:h-[600px] h-[450px] object-cover" autoPlay loop muted>
+    <video className="absolute top-0 -z-10 w-full bg-slate-400 md:h-[600px] h-[450px] object-cover" autoPlay loop muted onLoadedData={() => setLoad(false)}>
       <source src={videoSrc} type="video/mp4" />
     </video>
   );
